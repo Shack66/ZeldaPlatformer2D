f099bec1552025eeee1f53cb23a873f57a3c38bf
Shack66 <samuelarosemena@gmail.com>
2026-03-25 00:11:56 -0500

    Implement player health bar UI
    - Added new health bar spritesheet including gold frame, filled health, and empty health bar variants.
    - Integrated Slider component for player health, configured with a background (empty) and fill (active) layer system.
    - Updated CharacterTookDamage and CharacterHealed events to dynamically update the playerHealthSlider value based on the Player's health state.

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
@@ -9,9 +10,15 @@ public class UIManager : MonoBehaviour
 
     public Canvas gameCanvas;
 
+    [Header("Player Health  UI")]
+    public Slider playerHealthSlider;
+
     private void Awake()
     {
-        gameCanvas = Object.FindFirstObjectByType<Canvas>();
+        if (gameCanvas == null)
+        {
+            gameCanvas = Object.FindFirstObjectByType<Canvas>();
+        }
     }
 
     private void OnEnable()
@@ -52,16 +59,38 @@ public class UIManager : MonoBehaviour
             float randomX = Random.Range(-100f, 100f); // Side push
             healthText.moveSpeed = new Vector3(randomX, Random.Range(100f, 150f), 0);
         }
+
+        // Verify if the character damaged is the Player (Link)
+        if (character.CompareTag("Player") && playerHealthSlider != null)
+        {
+            Damageable playerHealth = character.GetComponent<Damageable>();
+
+            if (playerHealth != null)
+            {
+                // Update Slider value
+                playerHealthSlider.value = playerHealth.Health;
+            }
+        }
     }
 
     public void CharacterHealed(GameObject character, int healthRestored) 
     {
         // Create text at character hit
         Vector3 spawnPosition = Camera.main.WorldToScreenPoint(character.transform.position);
-
         TMP_Text tmpText = Instantiate(healthTextPrefab, spawnPosition, Quaternion.identity, gameCanvas.transform).GetComponent<TMP_Text>();
-
         tmpText.text = healthRestored.ToString();
+
+        // Update Health Bar
+        if (character.CompareTag("Player") && playerHealthSlider != null)
+        {
+            Damageable playerHealth = character.GetComponent<Damageable>();
+
+            if (playerHealth != null)
+            {
+                // Update Slider value
+                playerHealthSlider.value = playerHealth.Health;
+            }
+        }
     }
 
     public void OnExitGame(InputAction.CallbackContext context)
